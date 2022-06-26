4a019d23114721353f62c641cb8049ea2ad9e1e6
FAL-coffee <fal.engineer.2001@gmail.com>
2022-06-26 17:42:22 +0900

    fix ts-jestでsolidコンポーネントが動くように改修

---
+++ test/Button.tsx
@@ -0,0 +1,25 @@
+import type { Component, JSX } from "solid-js";
+import { mergeProps } from "solid-js";
+
+function withDefaults<P = {}>(Comp: Component<P>, defaultProps: Partial<P>) {
+  return ((props: any) => (
+    <Comp {...mergeProps(defaultProps, props)} />
+  )) as Component<P>;
+}
+export type ButtonProps = {} & (
+  | // Must specify icon, children or both - but never none of them
+  {
+      icon: JSX.Element;
+      children?: never;
+    }
+  | { icon?: never; children: any }
+  | { icon: JSX.Element; children: any }
+);
+
+const Button: Component<ButtonProps> = (props) => {
+  const { children } = props;
+
+  return <button>{children}</button>;
+};
+
+export default withDefaults(Button, {});
